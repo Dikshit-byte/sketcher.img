08858dca1546d22693694f8742d443b876681d8e
Dikshit <singhdikshit69@gmail.com>
2022-12-12 22:14:45 +0530

    completed text to img generation testing

--- index.py
+++ index.py
@@ -1,16 +1,18 @@
 import os
 import pprint 
 import openai
+import pyperclip as pc
 openai.organization = "org-XY7tvMhByFMbnaHiVRXoQjab"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.Model.list()
 
 response = openai.Image.create(
-  prompt="a white siamese cat",
+  prompt="lakshadweep island with blue water",
   n=1,
   size="1024x1024"
 )
 
 # pprint.pprint(dict(response),width=1)
 
-print(response[0])
+img_url = response['data'][0]['url']
+pc.copy(img_url)    
