7006b052366400c94a10d77a1b8b66260469befc
Dikshit <singhdikshit69@gmail.com>
2022-12-15 10:46:05 +0530

    best gpt ever

--- whisper/text.py
+++ whisper/text.py
@@ -3,13 +3,17 @@ import openai
 # import gradio as gr
 # from typer import prompt
 
+openai.organization = "org-XY7tvMhByFMbnaHiVRXoQjab"
 openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
+
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
 
-prompt = input("Ask me anything ? ")
-response = openai.Completion.create(
+while(True):
+    prompt = input("\nAsk me anything ? ")
+    response = openai.Completion.create(
     model="text-davinci-003",
     prompt=prompt,
     temperature=0.9,
@@ -19,9 +23,8 @@ response = openai.Completion.create(
     presence_penalty=0.6,
     stop=[" Human:", " AI:"]
     )
-result = response.choices[0].text
-
-for char in result: 
-    sys.stdout.write(char)
-    sys.stdout.flush()
-    time.sleep(0.01)
+    result = response.choices[0].text
+    for char in result: 
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.01)
